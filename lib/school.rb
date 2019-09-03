4a4709cf05720da2ca422020a00f505a9604dc83
John Karrell Speck <speckworks@gmail.com>
2019-09-03 14:01:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School 
+    attr_accessor :name, :grade
+    
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+    roster ={}
+  end
+
+  def add_student(name, grade)
+   roster[@name=name, @grade=grade]
+  end
+end
+
+    # code here!
